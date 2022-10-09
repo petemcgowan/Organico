f8fcab2fbdbee9a3a751c043393e2e5091d2213b
Pete <peteburquette@gmail.com>
2022-10-09 04:14:18 +0100

    Initial Commit

---
+++ Organico/OrganicoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrganicoApp.swift
+//  Organico
+//
+//  Created by Peter McGowan on 09/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct OrganicoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
