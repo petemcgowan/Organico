4f1004483286d6e33dc3588ef94d6ce1bd5484bc
Pete <peteburquette@gmail.com>
2022-10-21 23:00:45 +0100

    Passing array data around (tests)

--- Organico/ControlView.swift
+++ Organico/ControlView.swift
@@ -3,10 +3,14 @@ import SwiftUI
 
 struct ControlView: View {
     @Binding var lightIsOn: Bool
-    @Binding var groomBulb: GroomingItem
-
+    var writer: Writer
+    var writerOfArray: Writer
+    var groomingOfArray: GroomingItem
 
     var body: some View {
+        let displayId = String (groomingOfArray.id)
+        let displayPrice = String (groomingOfArray.price)
+        
         VStack(spacing: 20.0) {
             Button(action: {
                 lightIsOn.toggle()
@@ -16,15 +20,42 @@ struct ControlView: View {
             })
             Text("Turn the light bulb \(!lightIsOn ? "on" : "off")")
                 .fontWeight(.bold)
+            Text(writerOfArray.name)
+            Text(writerOfArray.surname)
+            Text(writerOfArray.website)
+            Text(writerOfArray.twitterNickname)
+            Text(displayId)
+            Text(groomingOfArray.title)
+            Text(groomingOfArray.productImage)
+            Text(displayPrice)
         }
         .navigationTitle("Control Room")
     }
 }
 
 struct ControlView_Previews: PreviewProvider {
+    static let writerPreview = Writer(
+        name: "Tiago",
+        surname: "Pereira",
+        website: "createwithswift.com",
+        twitterNickname: "@tiagogogo"
+    )
+    static let writerOfArrayPreview = Writer(
+        name: "Tiago",
+        surname: "Pereira",
+        website: "createwithswift.com",
+        twitterNickname: "@tiagogogo"
+    )
+    
+    static let groomingOfArrayPreview = GroomingItem(id: 6,
+                 title: "BetterYou Magnesium Body Lotion (180ml)",
+                 productImage: "BetterYou_Magnesium_Body_Lotion_180ml",
+                 price:9.99
+    )
+
     static var previews: some View {
         NavigationView {
-            ControlView(lightIsOn: .constant(true),id: id)
+            ControlView(lightIsOn: .constant(true), writer: writerPreview, writerOfArray: writerOfArrayPreview, groomingOfArray: groomingOfArrayPreview)
         }
     }
 }
--- Organico/SecondView.swift
+++ Organico/SecondView.swift
@@ -4,6 +4,7 @@ import SwiftUI
 
 struct SecondView: View {
     var writer: Writer
+    var writerArray: [Writer] = []
     
     var body: some View {
         VStack {
@@ -15,17 +16,23 @@ struct SecondView: View {
     }
 }
 
-struct SecondView_Previews: PreviewProvider {
-    // 5. Creare a static instance of Writer for your preview
-    static let writerPreview = Writer(
-        name: "Tiago",
-        surname: "Pereira",
-        website: "createwithswift.com",
-        twitterNickname: "@tiagogogo"
-    )
-    
-    static var previews: some View {
-        // 5. Use the right SecondView initializator
-        SecondView(writer: writerPreview)
-    }
-}
+//struct SecondView_Previews: PreviewProvider {
+//    // 5. Creare a static instance of Writer for your preview
+//    static let writerPreview = Writer(
+//        name: "Tiago",
+//        surname: "Pereira",
+//        website: "createwithswift.com",
+//        twitterNickname: "@tiagogogo"
+//    )
+//    static let writerOfArrayPreview = Writer(
+//        name: "Tiago",
+//        surname: "Pereira",
+//        website: "createwithswift.com",
+//        twitterNickname: "@tiagogogo"
+//    )
+//
+//    static var previews: some View {
+//        // 5. Use the right SecondView initializator
+//        SecondView(writer: writerPreview, writerOfArrayPreview)
+//    }
+//}
