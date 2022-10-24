13951a6a7cae136863e3bd7d2138488b7749cd2a
Pete <peteburquette@gmail.com>
2022-10-24 21:44:33 +0100

    Basic cart and payment button classes

---
+++ Organico/Components/CartButton.swift
@@ -0,0 +1,34 @@
+//
+//  CartButton.swift
+//  Organico
+//
+//  Created by Peter McGowan on 22/10/2022.
+//
+
+import SwiftUI
+
+struct CartButton: View {
+    var numberOfGroomingItems: Int
+    
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            Image(systemName: "cart")
+                .padding(.top, 5)
+
+            if numberOfGroomingItems > 0 {
+                Text("\(numberOfGroomingItems)")
+                    .font(.caption2).bold()
+                    .foregroundColor(.white)
+                    .frame(width: 15, height: 15)
+                    .background(Color(hue: 1.0, saturation: 0.89, brightness: 0.835))
+                    .cornerRadius(50)
+            }
+        }
+    }
+}
+
+struct CartButton_Previews: PreviewProvider {
+    static var previews: some View {
+        CartButton(numberOfGroomingItems: 1)
+    }
+}
