fcff1d6233e4ebf4bc3555c1681dcd359dbad8ad
Pete <peteburquette@gmail.com>
2023-01-16 20:12:25 +0000

    REST Posts

--- Organico/Components/WebView.swift
+++ Organico/Components/WebView.swift
@@ -1,24 +1,63 @@
-//
-//  WebView.swift
-//  Organico
-//
-//  Created by Peter McGowan on 05/01/2023.
-//
+
 
 import SwiftUI
 import WebKit
 
 struct WebView: UIViewRepresentable {
-    var url: URL
+//    var url: URL
+    var htmlFileName: String
     
-    func makeUIView (context: Context) -> WKWebView {
-        return WKWebView()
+    private let webView = WKWebView()
+
+    func makeUIView (context: Context) -> some UIView {
+        return webView
     }
-    
-    func updateUIView (_ webView: WKWebView, context: Context) {
-        let request = URLRequest (url: url)
-        webView.load(request)
+    func updateUIView (_ uiView: UIViewType, context: Context) {
+        webView.load(htmlFileName)
     }
-    
-    
+    //    func makeUIView (context: Context) -> WKWebView {
+    //        return WKWebView()
+    //    }
+
+//    func updateUIView (_ webView: WKWebView, context: Context) {
+//        let request = URLRequest (url: url)
+//        webView.load(request)
+//    }
+  
+    // this for augmenting the html file after it's loaded e.g. removing header/footer
+//    public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+//        let cssString = "page-header {display: none;}"
+//        let script = "var style = document.createElement('style'); style.innerHTML = '\(cssString)'; document.head.appendChild(style);"
+//        webView.evaluateJavaScript(script,
+//                                   completionHandler: { (response, error) -> Void in
+//            print(error!)
+//            webView.isHidden = false
+//        })
+//        let cssString1 = "page-bottom {display: none;}"
+//        let script1 = "var style = document.createElement('style'); style.innerHTML = '\(cssString1)'; document.head.appendChild(style);"
+//        webView.evaluateJavaScript(script1,
+//                                   completionHandler: { (response, error) -> Void in
+//            webView.isHidden = false
+//        })
+//    }
+}
+
+extension WKWebView {
+    func load (_ htmlFileName: String) {
+        guard !htmlFileName.isEmpty else {
+            return print("Empty file name")
+        }
+        
+        guard let filePath = Bundle.main.path(forResource: htmlFileName,
+                                              ofType: "html") else {
+                return print ("Error file path")
+        }
+        
+        do {
+            let htmlString = try String(contentsOfFile: filePath, encoding: .utf8)
+            loadHTMLString(htmlString, baseURL: URL(fileURLWithPath: filePath))
+        } catch {
+            print("error here")
+        }
+    }
 }
--- Organico/Screens/NewsletterView.swift
+++ Organico/Screens/NewsletterView.swift
@@ -1,14 +1,39 @@
-//
-//  NewsletterView.swift
-//  Organico
-//
-//  Created by Peter McGowan on 11/01/2023.
-//
+
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [HttpResult]
+}
+
+struct HttpResult: Codable {
+    var userId: Int
+    var id: Int
+    var title: String
+    var completed: Bool
+}
+
+struct PostModel: Decodable {
+    let id: Int
+    let userId: Int
+    let title: String
+    let body: String
+}
+
 struct NewsletterView: View {
     @EnvironmentObject var menuOpenedEnv: MenuOpened
+    @State private var fname: String = ""
+    @State private var lname: String = ""
+    @State private var eaddress: String = ""
+    
+    @State private var results = [HttpResult]()
+
+    let jsonURL = "https://jsonplaceholder.typicode.com/todos"
+    let jsonPostURL = "https://jsonplaceholder.typicode.com/posts"
+    // JSONP
+    let newsletterURLJson = "https://organico.us1.list-manage.com/subscribe/post-json?u=78152c6223edaed0ca0056c1a&id=151c1fac43&c=?"
+    let newsletterURL = "https://organico.us1.list-manage.com/subscribe/post?u=78152c6223edaed0ca0056c1a&id=151c1fac43"
+    let newsletterURLPlain = "https://organico.us1.list-manage.com/subscribe/post"
     
     var body: some View {
         VStack {
@@ -37,39 +62,171 @@ struct NewsletterView: View {
                 Spacer()
                 Spacer()
 
-                Section {
-                    submit
-                }
+                Button(action: {
+                    // Here's what they've entered
+                    print("fname: \(fname)")
+                    print("lname: \(lname)")
+                    print("eaddress: \(eaddress)")
+//                    loadData(fromURL: jsonURL)
+//                    postDataOdenza()
+                    postDataNewsletter()
+//                    Task {
+//                        await submitToNewsletter()
+//                    }
+                }, label: {
+                    Text ("Submit")
+                })
+                
             }
-            .font(Font.custom("Nobile-Regular", size: 16))
         } // vstack
+        .font(Font.custom("Nobile-Regular", size: 16))
+        .background(Color("SafeAreaBackgroundColor"))
         .onAppear {
             print("Newsletter Page appeared")
             menuOpenedEnv.menuOpened = false
         }
         //subscribe button
     }
+    
+    func loadData(fromURL url:String) {
+        guard let url = URL(string: url) else {
+            print("Invalid URL")
+            return
+        }
+
+        let request = URLRequest(url: url)
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let decodedResponse = try? JSONDecoder().decode([HttpResult].self, from: data) {
+                    // we have good data – go back to the main thread
+                    DispatchQueue.main.async {
+                        // update our UI
+                        self.results = decodedResponse
+                        print("Results", self.results)
+                    }
+
+                    // everything is good, so we can exit
+                    return
+                }
+            }
+
+            // if we're still here it means there was a problem
+            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+        }.resume()
+    }
+
+    func postDataNewsletter() {
+        guard let url = URL (string: newsletterURLPlain) else {return}
+        let emailAddressNL = "jellyumbrella@gmail.com"
+        let firstNameNL = "Jelly"
+        let lastNameNL = "Umbrella"
+        
+//        let body: [String: Any] = ["EMAIL": emailAddressNL, "FNAME": firstNameNL, "LNAME": lastNameNL]
+//        let body: [String: Any] = ["members": [ "email_address": emailAddressNL, "status": "subscribed", "merge_fields": [ "FNAME": firstNameNL, "LNAME": lastNameNL]]]
+        let body: [String: Any] = ["apikey": "78152c6223edaed0ca0056c1a", "id": "151c1fac43", "email": ["email":emailAddressNL, "euid":"", "leid": ""], "merge_vars": [ "FNAME": firstNameNL, "LNAME": lastNameNL], "double_optin": "false"]
+        print (body)
+
+        
+        let finalData = try! JSONSerialization.data(withJSONObject: body)
+        print (finalData)
+
+        var request = URLRequest (url: url)
+        request.httpMethod = "POST"
+        request.httpBody = finalData
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        print (request)
+        URLSession.shared.dataTask(with: request) { (data, res, err) in
+            do{
+                if let data = data {
+                    
+                    let result = try JSONDecoder().decode(PostModel.self, from: data)
+                    print (result)
+                } else {
+                    print("No data")
+                }
+                
+            } catch (let error) {
+                print ("error:", error.localizedDescription)
+            }
+        }.resume()
+    }
+    
+    func postDataOdenza() {
+        guard let url = URL (string: jsonPostURL) else {return}
+        let title = "foo"
+        let bar = "bar"
+        let userId = 1
+        
+        let body: [String: Any] = ["title": title, "body": bar, "userId": userId]
+        
+        let finalData = try! JSONSerialization.data(withJSONObject: body)
+        
+        var request = URLRequest (url: url)
+        request.httpMethod = "POST"
+        request.httpBody = finalData
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        print (request)
+        print (finalData)
+        URLSession.shared.dataTask(with: request) { (data, res, err) in
+            do{
+                if let data = data {
+                    
+                    let result = try JSONDecoder().decode(PostModel.self, from: data)
+                    print (result)
+                } else {
+                    print("No data")
+                }
+                
+            } catch (let error) {
+                print ("error:", error.localizedDescription)
+            }
+        }.resume()
+    }
+    
+    func postData(fromURL url:String) {
+        guard let url = URL(string: jsonPostURL) else {
+            print("Invalid URL")
+            return
+        }
+
+        let request = URLRequest(url: url)
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let decodedResponse = try? JSONDecoder().decode([HttpResult].self, from: data) {
+                    // we have good data – go back to the main thread
+                    DispatchQueue.main.async {
+                        // update our UI
+                        self.results = decodedResponse
+                        print("Results", self.results)
+                    }
+
+                    // everything is good, so we can exit
+                    return
+                }
+            }
+
+            // if we're still here it means there was a problem
+            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+        }.resume()
+    }
+    
+
 }
+
 
 private extension NewsletterView {
     var emailAddress: some View {
-        TextField("Email Address", text: .constant(""))
+        TextField("Email Address", text: $fname)
     }
 
     var firstname: some View {
-        TextField("First Name", text: .constant(""))
+        TextField("First Name", text: $lname)
     }
 
     var lastname: some View {
-        TextField("Last Name", text: .constant(""))
-    }
-    
-    var submit: some View {
-        Link("Submit", destination: URL(string: "https://www.google.com")!)
-            .environment(\.openURL, OpenURLAction { url in
-                print("Open \(url)")
-                return .handled
-            })
+        TextField("Last Name", text: $eaddress)
     }
 }
 
